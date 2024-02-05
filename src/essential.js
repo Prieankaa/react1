36f4c060748e8aa0762be63bb0aa36308f33cc14
Prieankaa <prieankaofficial@gmail.com>
2024-02-05 11:07:45 +0530

    first commit

---
+++ src/essential.js
@@ -0,0 +1,8 @@
+function Essentials(){
+    return(
+        <div className="living">
+            <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/6/8/eff01060-f706-468d-b97c-95cdf43174f91528443826867-Desktop-Home-Banner.jpg" alt="" />
+        </div>
+    );
+}
+export default Essentials;
